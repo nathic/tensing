f66528106ccf5546a7ea0be9eb60918c05719124
Oliver Frischknecht <ofrischk@hsr.ch>
2015-04-26 21:49:05 +0200

    added models

---
+++ Tensing/app/src/main/java/ch/ronoli/tensing/models/Category.java
@@ -0,0 +1,39 @@
+package ch.ronoli.tensing.models;
+
+/**
+ * Created by nathic on 26.04.2015.
+ */
+public class Category {
+    private long id;
+    private String name;
+    private Type type;
+
+    public Category(String name, Type type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+}
---
+++ Tensing/app/src/main/java/ch/ronoli/tensing/models/Exercise.java
@@ -0,0 +1,72 @@
+package ch.ronoli.tensing.models;
+
+/**
+ * Created by nathic on 26.04.2015.
+ */
+public class Exercise {
+
+    private long id;
+    private String name;
+    private String text;
+    private String description;
+    private String thumbnail;
+    private String link;
+    private Category category;
+
+    public Exercise(String name, String text, String description, String thumbnail, String link, Category category){
+        this.name = name;
+        this.text = text;
+        this.description = description;
+        this.thumbnail = thumbnail;
+        this.link = link;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
